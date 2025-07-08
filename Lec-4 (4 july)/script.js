8679b7a9bab56e7a2e0b5dee4ddbe7ebe4e630cd
Pulkit Vashisht <vashishtpulkit68@gmail.com>
2025-07-08 15:44:34 +0530

    ok

--- Lec-4 (4 july)/script.js
+++ Lec-4 (4 july)/script.js
@@ -1,143 +1,9 @@
-// let userData = [
-//     { firstName: 'Priyanshu', age: '21' },
-//     { firstName: 'Pranav', age: '20' },
-//     { firstName: 'Divesh', age: '23' },
-// ]
-
-// // map and filter
-
-// let firstName = userData.map((data) => data.firstName)
-// let output = userData.filter((data) => data.age > 20)
-
-// console.log(firstName)
-// console.log(output)
-
-// PROMISES
-
-// const myPromise = new Promise((resolve, reject) => {
-//   setTimeout(() => {
-//     resolve("foo");
-//   }, 300);
-// });
-
-// const promise = new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//         reject('I GOT PLACED')
-//     }, 3000)
-// })
-// const promise = new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//         reject('I got rejected')
-//     }, 3000)
-// })
-
-// promise
-//     .then((res) => {
-//         console.log(res)
-//     })
-//     .catch((err) => {
-//         console.error('ERROR in first then: ', err)
-//     })
-//     .then(() => {
-//         console.log('PARTY TIME')
-//     })
-//     .catch((err) => {
-//         console.error('ERROR in second then: ', err)
-//     })
-//     .then(() => {
-//         console.log('WORKING WORKING WORKING')
-//     })
-//     .catch((err) => {
-//         console.error('ERROR in third then: ', err)
-//     })
-//     .then(() => {
-//         console.log('LAYOFF')
-//     })
-//     .catch((err) => {
-//         console.error('ERROR in fourth then: ', err)
-//     })
-
-// console.log(promise)
-// console.log(promise2)
-
-// const p1 = new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//         reject('ASSIGNMENT COMPLETED BY YASH')
-//     }, 2000)
-// })
-
-// const p2 = new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//         reject('ASSIGNMENT COMPLETED BY VINAYAK')
-//     }, 3000)
-// })
-
-// const p3 = new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//         reject('ASSIGNMENT COMPLETED BY VANSH')
-//     }, 5000)
-// })
-
-// const p4 = new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//         reject('ASSIGNMENT COMPLETED BY GARG')
-//     }, 4000)
-// })
-
-// Promise.all([p1, p2, p3, p4])
-//     .then((res) => {
-//         console.log(res)
-//     })
-//     .catch((err) => {
-//         console.log('ERROR OCCURED: ', err)
-//     })
-
-// Promise.allSettled([p1, p2, p3, p4])
-//     .then((res) => {
-//         console.log(res)
-//     })
-//     .catch((err) => {
-//         console.log('ERROR OCCURED: ', err)
-//     })
-
-// Promise.race([p1, p2, p3, p4])
-//     .then((res) => {
-//         console.log(res)
-//     })
-//     .catch((err) => {
-//         console.log('ERROR OCCURED: ', err)
-//     })
-
-// Promise.any([p1, p2, p3, p4])
-//     .then((res) => {
-//         console.log(res)
-//     })
-//     .catch((err) => {
-//         console.log('ERROR OCCURED: ', err)
-//     })
-
-// ASYNC AWAIT
-
-// const p1 = new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//         resolve('ASSIGNMENT COMPLETED BY VANSH')
-//     }, 2000)
-// })
-
-// async function checkAssignment() {
-//     const dataAssignment = await p1
-//     console.log('Checking ASSIGNMENT: ', dataAssignment)
-// }
-
-// checkAssignment()
-
-// FETCH
-
 async function fetchProducts() {
     try {
         const res = await fetch('https://dummyjson.com/products')
         const data = await res.json()
 
+        // Extract title, description, price from products array
         const products = data.products.map((product) => ({
             title: product.title,
             description: product.description,
@@ -151,3 +17,97 @@ async function fetchProducts() {
 }
 
 fetchProducts()
+
+
+
+
+// function welcomeStudents(welcomeMessage, goodbyeMessage) {
+//     console.log(
+//         welcomeMessage +
+//             ' ' +
+//             this.studentName +
+//             ' is a student of class ' +
+//             this.studentClass +
+//             ' ' +
+//             goodbyeMessage
+//     )
+// }
+
+// const student1 = {
+//     studentName: 'Yash',
+//     studentClass: 'G4',
+// }
+// const student2 = {
+//     studentName: 'Arnav',
+//     studentClass: 'G6',
+// }
+
+// // CALL
+
+// welcomeStudents.call(student1, 'Good Morning', 'Bye')
+// welcomeStudents.call(student2, 'Good Evening', 'Bye')
+
+// // BIND
+// welcomeStudents.bind(student1, 'Good Morning', 'Bye')
+// welcomeStudents.bind(student2, 'Good Evening', 'Bye')
+
+// // // NORMAL FUNCTION
+
+// // function sumOfThree(a, b, c) {
+// //     console.log(a + b + c)
+// // }
+
+// // sumOfThree(1, 2, 3)
+
+// // // CURRIED FUNCTION
+
+// // function sumOfThree(a) {
+// //     return function (b) {
+// //         return function (c) {
+// //             return a + b + c
+// //         }
+// //     }
+// // }
+
+// // console.log(sumOfThree(2)(4)(6))
+
+// // EVENT CAPTURING & EVENT BUBBLING
+
+// const grandParent = document.getElementById('grandParent')
+// const parent = document.getElementById('parent')
+// const child = document.getElementById('child')
+
+// grandParent.addEventListener('click', () => {
+//     console.log('GRANDPARENT CLICKED')
+// })
+
+// parent.addEventListener('click', () => {
+//     console.log('PARENT CLICKED')
+// })
+
+// child.addEventListener('click', () => {
+//     console.log('CHILD CLICKED')
+// })
+
+
+
+
+// grandParent.addEventListener(
+//     'click',
+//     () => {
+//         console.log('GRANDPARENT CLICKED')
+//     },
+//     true
+// )
+
+// parent.addEventListener(
+//     'click',
+//     () => {
+//         console.log('PARENT CLICKED')
+//     },
+//     true
+// )
+
+// child.addEventListener('click', () => {
+//     console.log('CHILD CLICKED')
+// })
